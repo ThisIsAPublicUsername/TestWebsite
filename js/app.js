aabae3b53d37c707c71bb4b9343ec10580218dec
ThisIsAPublicUsername <128112325+ThisIsAPublicUsername@users.noreply.github.com>
2023-05-28 20:12:29 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,58 @@
 $(document).ready(function() {
-  $('.clickableImage').click(function() {
-    $(this).toggleClass("big");
+  var scaleXValueBy, scaleYValueBy;
 
-  });
-});
+  console.log(window.innerWidth);
+
+  if(window.innerWidth <= 200)
+  {
+    scaleXValueBy = 0.55;
+    scaleYValueBy = 0.55;
+  }
+  else if(window.innerWidth <= 700)
+  {
+    scaleXValueBy = 1.30;
+    scaleYValueBy = 1.30;
+  }
+  else if(window.innerWidth <= 1100)
+  {
+    scaleXValueBy = 1.65;
+    scaleYValueBy = 1.65;
+  }
+  else
+  {
+    scaleXValueBy = 3.00;
+    scaleYValueBy = 2.55;
+  }
+  
+  var horizontalCenter, verticalCenter;
+
+  var clone;
+
+  $('.clickableImage').click(function() {
+  var centeredDiv = document.createElement('div');
+  centeredDiv.id = "centeredDivID";
+  centeredDiv.className = "centeredDivClass";
+  centeredDiv.style.overflow='auto';
+  document.getElementsByTagName('body')[0].appendChild(centeredDiv);
+
+  clone = $(this)[0].cloneNode(false);
+  clone.setAttribute('id',"clonedElement");
+  clone.style.cursor = "pointer";
+   centeredDiv.appendChild(clone);
+
+  horizontalCenter = (window.innerWidth / 2) - (clone.width / 2);
+  verticalCenter = (window.innerHeight / 2) - (clone.height / 2);
+
+  clone.style.left = horizontalCenter + 'px';
+  clone.style.top = verticalCenter + 'px';
+  clone.style.transform = "scale(" + scaleXValueBy + "," + scaleYValueBy + ")";
+  clone.style.zIndex = "20";
+
+  document.getElementById('allImagesDiv').style.opacity = 0.25;
+
+  clone.onclick = function() {
+    centeredDiv.remove();
+    document.getElementById('allImagesDiv').style.opacity = 1;
+  }; // end onclick() function
+  }); // end click() function
+}); // end ready()
